d11ab37ba1caa09968bf4b8776c52253ff2ae8c3
tguyer <tguyer@radford.edu>
2016-11-03 14:37:05 -0400

    main method/controller made

---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+
+public class Driver {
+
+	public static void main(String[] args){
+		String input = "";
+		while (!input.equalsIgnoreCase("quit") || !input.equalsIgnoreCase("exit")){
+			
+		}
+	}
+}
